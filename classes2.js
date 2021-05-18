069afbe5dde9a27c23879d2e20b991c27fcd979e
mariagluck <mariamart.berlin@gmail.com>
2021-05-18 09:41:47 +0200

    adding versions

--- classes2.js
+++ classes2.js
@@ -31,8 +31,22 @@
  const BlimpFactory = factoryFactory("blimp");
  const TrainFactory = factoryFactory("train");
 
+ //print them in array
+ const bicycle1 = new BikeFactory("Pinnarello","Dogma_12");
+ const car1 = new CarFactory("Mercedes Benz","Smart");
+ const boat1 = new BoatFactory("Bertram","35");
+ const blimp1 = new BlimpFactory("Goodyear","A-170L");
+ const train1 = new TrainFactory("Talgo","230");
 
- const allVehicles = [];
+const allVehicles = [
+        car1,
+        boat1,
+        blimp1,
+        train1
+];
+
+//or like this
+ const allVehicles1 = [];
 
  allVehicles.push(new BikeFactory("Pinnarello","Dogma_12"));
  allVehicles.push(new CarFactory("Mercedes Benz","Smart"));
@@ -40,18 +54,23 @@
  allVehicles.push(new BlimpFactory("Goodyear","A-170L"));
  allVehicles.push(new TrainFactory("Talgo","230"));
 
+
 //print with loop all vehicles in the array
  for(vehicle in allVehicles){
     console.log(allVehicles[vehicle]);
 }
 
+//or like this
+allVehicles1.forEach(function(vehicle) {
+    console.log("vehicle", vehicle);
+});
 
 // Question: Check the *type* of your factories and of the objects that your factories create - what is the type of a factory and what is the type of a created object? Add your answer as comments into into your solution file.
-console.log(typeof bikeFactory); // function
-console.log(typeof carFactory); // function
-console.log(typeof boatFactory); // function
-console.log(typeof blimpFactory); // function
-console.log(typeof trainFactory); // function
+console.log(typeof BikeFactory); // function
+console.log(typeof CarFactory); // function
+console.log(typeof BoatFactory); // function
+console.log(typeof BlimpFactory); // function
+console.log(typeof TrainFactory); // function
 
 console.log(typeof allVehicles); //object
 
