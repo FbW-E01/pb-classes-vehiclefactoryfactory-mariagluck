bb6c9e281e1b6ce7b743a000186e6d1c03fbe817
mariagluck <mariamart.berlin@gmail.com>
2021-05-18 09:28:43 +0200

    cleaning code

--- classes2.js
+++ classes2.js
@@ -16,37 +16,37 @@
 
 // Store all your created vehicles inside one array, loop over that array and print out each vehicle.
 
-// Question: Check the *type* of your factories and of the objects that your factories create - what is the type of a factory and what is the type of a created object? Add your answer as comments into into your solution file.
-
  function factoryFactory(type) {
 
-    return function ConstructorFunction(make, model) {
+    return function ConstructorFactory(make, model) {
        this.type = type;
        this.make = make;
        this.model = model;
     }
  }
  
- const bikeFactory = factoryFactory("bicycle");
- const carFactory = factoryFactory("car");
- const boatFactory = factoryFactory("boat");
- const blimpFactory = factoryFactory("blimp");
- const trainFactory = factoryFactory("train");
+ const BikeFactory = factoryFactory("bicycle");
+ const CarFactory = factoryFactory("car");
+ const BoatFactory = factoryFactory("boat");
+ const BlimpFactory = factoryFactory("blimp");
+ const TrainFactory = factoryFactory("train");
 
 
  const allVehicles = [];
 
- allVehicles.push(new bikeFactory("Pinnarello","Dogma_12"));
- allVehicles.push(new carFactory("Mercedes Benz","Smart"));
- allVehicles.push(new boatFactory("Bertram","35"));
- allVehicles.push(new blimpFactory("Goodyear","A-170L"));
- allVehicles.push(new trainFactory("Talgo","230"));
+ allVehicles.push(new BikeFactory("Pinnarello","Dogma_12"));
+ allVehicles.push(new CarFactory("Mercedes Benz","Smart"));
+ allVehicles.push(new BoatFactory("Bertram","35"));
+ allVehicles.push(new BlimpFactory("Goodyear","A-170L"));
+ allVehicles.push(new TrainFactory("Talgo","230"));
 
 //print with loop all vehicles in the array
  for(vehicle in allVehicles){
     console.log(allVehicles[vehicle]);
 }
 
+
+// Question: Check the *type* of your factories and of the objects that your factories create - what is the type of a factory and what is the type of a created object? Add your answer as comments into into your solution file.
 console.log(typeof bikeFactory); // function
 console.log(typeof carFactory); // function
 console.log(typeof boatFactory); // function
@@ -54,10 +54,6 @@ console.log(typeof blimpFactory); // function
 console.log(typeof trainFactory); // function
 
 console.log(typeof allVehicles); //object
-
-
-
-
 
 
 
